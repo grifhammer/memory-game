5ebbb31593412d8d037c99df3f66acdcec89f99c
Griffin Hammer <grifhammer@gmail.com>
2015-10-26 10:56:24 -0400

    Renamed numMoves to numClicks
    Changed to make the name more accurate to what it counts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
 
-	for(var i =0; i< 20; i++){
+	for(var i =0; i< 25; i++){
 		var rand = Math.floor(Math.random() * 8 + 1);
 		$('.mg_tile-' + rand ).appendTo($('.mg_contents'))
 	}
 
-	var numMoves = 0;
+	var numClicks = 0;
 
     $('.mg_tile').click(function(){
 
-
+    	// Determines if there are two unmatched cards showing
+    	// 		if so hide them
     	unmatchedVisibleTiles = $('.mg_tile-inside:visible.unmatched')
         if(unmatchedVisibleTiles.length == 2){
             unmatchedVisibleTiles.hide();
-            numMoves +=2;
+            numClicks +=2;
         }
+
+        //Displays tile clicked on
     	$(this).find('.mg_tile-inside').show();
         
         var visibleTiles = $('.mg_tile-inside:visible');
-        //needs to be changed to not hide matched 
         var unmatchedVisibleTiles = $('.mg_tile-inside:visible.unmatched')
+
+ 		// Checks for match 
         if(unmatchedVisibleTiles.length == 2){
         	var lastImgSrc = '';
         	var lastTile;
@@ -29,7 +33,7 @@ $(document).ready(function(){
         			$(this).removeClass('unmatched')
         			lastTile.addClass('matched')
         			$(this).addClass('matched')
-        			numMoves+=2;
+        			numClicks+=2;
         		}
         		lastTile = $(this);
         		lastImgSrc = $(this).find('img').attr('src')
@@ -37,7 +41,7 @@ $(document).ready(function(){
         }
         
         if($('.unmatched').length == 0){
-        	alert('You Won! (In ' + numMoves + ' moves!)');
+        	alert('You Won! (In ' + numClicks + ' clicks!)');
         }
     });
 });
